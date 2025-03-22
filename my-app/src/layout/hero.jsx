6863feaf2c99637cd8baa827b7ec42c52b1e0bae
DiegoJimenezzzM <diegojimo2005@gmail.com>
2025-03-22 12:57:19 -0500

    Avances en el hero

--- my-app/src/layout/hero.jsx
+++ my-app/src/layout/hero.jsx
@@ -2,14 +2,17 @@ import backgroundImage from '../assets/walling-XLqiL-rz4V8-unsplash.jpg';
 
 export default function Hero() {
     return (
-        <div className="hero">
-            <img src={backgroundImage} alt="Background" className='imgFondo' />
+        <div  className="hero">
             <div className="hero__content">
-                <h1>Tu sitio web listo para despegar</h1>
-                <p>En JVDataSoft , diseñamos sitios
+                <h1 className='titulo'>Tu sitio web listo para despegar</h1>
+
+                <p className='texto'>
+                    En JVDataSoft , diseñamos sitios
                     profesionales, rápidos
-                    y optimizados.</p>
-                <a href="#contacto" className="btn">Contáctanos</a>
+                    y optimizados.
+                </p>
+
+                <a href="#contacto" className="btn">¡Contáctanos!</a>
             </div>
         </div>
     )
