2ab70f5fca6a0780a793645e632fb366207b9642
DiegoJimenezzzM <diegojimo2005@gmail.com>
2025-03-23 10:03:15 -0500

    Seccion Hero finalizada

--- my-app/src/layout/AboutUs.jsx
+++ my-app/src/layout/AboutUs.jsx
@@ -3,7 +3,13 @@ export default function SobreNosotros() {
         <div>
             <h2>Sobre Nosotros</h2>
             <div className="sobre-nosotros-info">
-                <p>Impulsa tu negocio con un sitio web profesional. En JVDataSoft, creamos páginas web atractivas, rápidas y optimizadas para que conviertas más clientes. Desde tiendas online hasta sitios corporativos, diseñamos soluciones a tu medida con integración de pago, SEO y soporte continuo.</p>
+                <p>
+                    Impulsa tu negocio con un sitio web profesional. 
+                    En JVDataSoft, creamos páginas web atractivas, rápidas
+                    y optimizadas para que conviertas más clientes. Desde 
+                    tiendas online hasta sitios corporativos,
+                    diseñamos soluciones a tu medida con integración de pago,
+                    SEO y soporte continuo.</p>
                 <img src="" alt="logo" className="img-sobre-nosotros" />
             </div>
         </div>
--- my-app/src/layout/hero.jsx
+++ my-app/src/layout/hero.jsx
@@ -1,18 +1,17 @@
-import backgroundImage from '../assets/walling-XLqiL-rz4V8-unsplash.jpg';
-
 export default function Hero() {
     return (
-        <div  className="hero">
+        <div className="hero">
+            <img src="../../../src/assets/walling-XLqiL-rz4V8-unsplash.jpg" alt="Fondo" className="hero__bg"/>
+            <div className="hero__overlay"></div> 
+
             <div className="hero__content">
                 <h1 className='titulo'>Tu sitio web listo para despegar</h1>
-
                 <p className='texto'>
-                    En JVDataSoft , diseñamos sitios
-                    profesionales, rápidos
-                    y optimizados.
+                    En JVData<span className='soft'>Soft</span>, diseñamos sitios profesionales, rápidos y optimizados.
                 </p>
-
-                <a href="#contacto" className="btn">¡Contáctanos!</a>
+                <button className="boton">
+                    <a href="#contacto" className='botontx'>¡Contáctanos!</a>
+                </button>
             </div>
         </div>
     )
