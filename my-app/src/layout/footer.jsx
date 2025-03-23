0ac4cb0899589ceeb2d68d42773f132bda432941
Jheyfer Aguilar <jheyferaguilar@gmail.com>
2025-03-23 13:18:14 -0500

    Footer finalizado

--- my-app/src/layout/footer.jsx
+++ my-app/src/layout/footer.jsx
@@ -2,16 +2,16 @@ export default function Footer() {
 
     return (
         <footer className="footer">
-            <div>
-                <h3>Contacto</h3>
+            <div className="footer__contact">
+                <h3>Siguenos</h3>
                 <ul>
-                    <li><i class="fa-brands fa-whatsapp"></i></li>
-                    <li><i class="fa-brands fa-x-twitter"></i></li>
-                    <li><i class="fa-brands fa-facebook-f"></i></li>
-                    <li><i class="fa-brands fa-instagram"></i></li>
+                    <li><a href="https://wa.me/message/TVDN3JXWYDNBO1" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
+                    <li><a href="https://x.com/JvdataSoft" target="_blank"><i class="fa-brands fa-x-twitter"></i> </a></li>
+                    <li><a href="https://www.facebook.com/profile.php?id=61573578434971" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
                 </ul>
             </div>
-            <p>Todos los derechos reservados  &#169;. JVDataSoft {new Date().getFullYear()}</p>
+            <p>Todos los derechos reservados  <b>&#169;</b>. JVData<b>Soft</b> {new Date().getFullYear()}</p>
         </footer>
     )
 
