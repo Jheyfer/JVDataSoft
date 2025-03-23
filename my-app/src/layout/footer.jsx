218efaeb98e2c30ae260528864d91c5f393d0fa3
Jheyfer Aguilar <jheyferaguilar@gmail.com>
2025-03-23 11:48:51 -0500

    avances footer

--- my-app/src/layout/footer.jsx
+++ my-app/src/layout/footer.jsx
@@ -1,7 +1,16 @@
 export default function Footer() {
 
     return (
-        <footer>
+        <footer className="footer">
+            <div>
+                <h3>Contacto</h3>
+                <ul>
+                    <li><i class="fa-brands fa-whatsapp"></i></li>
+                    <li><i class="fa-brands fa-x-twitter"></i></li>
+                    <li><i class="fa-brands fa-facebook-f"></i></li>
+                    <li><i class="fa-brands fa-instagram"></i></li>
+                </ul>
+            </div>
             <p>Todos los derechos reservados  &#169;. JVDataSoft {new Date().getFullYear()}</p>
         </footer>
     )
