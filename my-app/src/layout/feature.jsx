6f0670b352dac4ce1b8d65af91c8bf4c30752985
DiegoJimenezzzM <diegojimo2005@gmail.com>
2025-03-23 11:46:07 -0500

    Li sin style y adelanto del feature

--- my-app/src/layout/feature.jsx
+++ my-app/src/layout/feature.jsx
@@ -1,39 +1,40 @@
 export default function feature() {
     return (
-        <div>
-            <h2>Caracteristicas de la empresa</h2>
-            <ul>
-                <li>
+        <div className="feature">
+            <h2 className="titulo">Características de la empresa</h2>
+
+            <ul className="lista">
+                <li className="item">
                     <div>
                         <i class="fa-solid fa-pen"></i>
                     </div>
                     <p><span> Diseño personalizado: </span>Adaptado a tu marca y objetivos.</p>
                 </li>
-                <li>
+                <li className="item">
                     <div>
                         <i class="fa-regular fa-clock"></i>
                     </div>
                     <p><span> Velocidad y optimización: </span>Carga rápida y SEO para más tráfico.</p>
                 </li>
-                <li>
+                <li className="item">
                     <div>
                         <i class="fa-solid fa-gear"></i>
                     </div>
                     <p><span>Seguridad y estabilidad: </span>Protección contra ataques y caídas.</p>
                 </li>
-                <li>
+                <li className="item">
                     <div>
                         <i class="fa-solid fa-wrench"></i>
                     </div>
                     <p><span>Funcionalidades a medida: </span>Tiendas online, reservas, blogs y más.</p>
                 </li>
-                <li>
+                <li className="item">
                     <div>
                         <i class="fa-thin fa-mobile"></i>
                     </div>
                     <p><span>100% Responsivo: </span>Se ve perfecto en computadoras y móviles.</p>
                 </li>
-                <li>
+                <li className="item">
                     <div>
                         <i class="fa-light fa-calendar"></i>
                     </div>
