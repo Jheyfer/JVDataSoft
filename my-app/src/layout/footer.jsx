e74535f88ef41d48750330e765b8d0c2cd1e3683
Jheyfer <130316907+Jheyfer@users.noreply.github.com>
2025-03-28 11:36:12 -0500

    Update footer.jsx

--- my-app/src/layout/footer.jsx
+++ my-app/src/layout/footer.jsx
@@ -8,7 +8,6 @@ export default function Footer() {
                     <li><a href="https://wa.me/message/TVDN3JXWYDNBO1" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
                     <li><a href="https://x.com/JvdataSoft" target="_blank"><i class="fa-brands fa-x-twitter"></i> </a></li>
                     <li><a href="https://www.facebook.com/share/168dF3hMSa/" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-                    <li><a href="#" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
                 </ul>
             </div>
             <p>Todos los derechos reservados  <b>&#169;</b>. JVData<b>Soft</b> {new Date().getFullYear()}</p>
